4be76f13e60062aa4eed94adb9a256d62eb91517
Shamil <noteness@riseup.net>
2023-06-21 12:02:15 +0530

    Better 2

--- Manylinux2014_Compliant_Source/pkg/install-deps.sh
+++ Manylinux2014_Compliant_Source/pkg/install-deps.sh
@@ -1,11 +1,17 @@
-wget https://apt.llvm.org/llvm.sh
-sudo bash llvm.sh 12
+yum install -y wget bash cmake
 
 mkdir build && cd build
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+tar -xvf https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+mv clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-16.04/* /usr/
+
 wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
 tar -xvzf eigen-3.3.7.tar.gz
 
 mkdir eigen-build
 cd eigen-build
 cmake ../eigen-3.3.7
-make -j$(nproc) && cd ..
+make -j$(nproc) && make install
+
+cd ..
+cmake ../src && make -j$(nproc) && make install
