4282276d66d7bcd2a0eec8199a332836c86602dd
Shamil <noteness@riseup.net>
2023-06-21 15:48:01 +0530

    Better4

--- Manylinux2014_Compliant_Source/pkg/install-deps.sh
+++ Manylinux2014_Compliant_Source/pkg/install-deps.sh
@@ -1,15 +1,16 @@
-yum install -y wget bash cmake
+yum install -y wget bash cmake ncurses-libs ncurses-devel
+
 mkdir build && cd build
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/llvm-12.0.0.src.tar.xz
-tar -xvf https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/llvm-12.0.0.src.tar.xz
-cd llvm-12.0.0
+wget -q https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/llvm-12.0.0.src.tar.xz
+tar -xf llvm-12.0.0.src.tar.xz
+cd llvm-12.0.0.src
 mkdir build
 cd build
 cmake ..
 make -j$(nproc)
 make install
 
-wget https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
+wget -q https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz
 tar -xvzf eigen-3.3.7.tar.gz
 
 mkdir eigen-build
@@ -17,5 +18,5 @@ cd eigen-build
 cmake ../eigen-3.3.7
 make -j$(nproc) && make install
 
-cd /project/build
-cmake ../src && make -j$(nproc) && make install
+mkdir /project/finalbuild && cd /project/finalbuild
+cmake /project/src && make -j$(nproc) && make install
